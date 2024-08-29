6446c2c73bb011a3d456bd24a2bb845fdbbceab9
SlavaMryasov <slavadevbox1@gmail.com>
2024-08-29 20:49:47 +0300

    UK-1 chore: added plugin tailwind and update rules for eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,8 @@
 module.exports = {
   extends: "@it-incubator/eslint-config",
-  rules: { "no-console": ["warn", { allow: ["warn", "error"] }] },
+  rules: { "no-console": ["warn", { allow: ["warn", "error"] }],
+  "react/jsx-curly-brace-presence": [
+    'error',
+    { children: 'never', propElementValues: 'never', props: 'never' },
+  ], },
 };
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,3 +1,5 @@
 module.exports = {
   ...require("@it-incubator/prettier-config"),
+  jsxSingleQuote: true,
+  plugins: ["prettier-plugin-tailwindcss"]
 };
